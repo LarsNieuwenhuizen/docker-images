a7d8dc2029818730d1e06cbf1213a4af24ff1916
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2021-10-28 00:45:46 +0200

    docker: Update docker files and build command

--- php/build.sh
+++ php/build.sh
@@ -22,14 +22,15 @@ fi
 for version in ${buildVersions[*]}; do
   DOCKER_BUILDKIT=1 docker build \
       --no-cache \
+      --target=production \
       -t larsnieuwenhuizen/php-fpm:${version} \
       --build-arg PHP_VERSION=${version} .
 
   DOCKER_BUILDKIT=1 docker build \
       --no-cache \
+      --target=development \
       -t larsnieuwenhuizen/php-fpm:${version}-dev \
-      --build-arg PHP_VERSION="${version}" \
-       --build-arg CONTEXT=development .
+      --build-arg PHP_VERSION="${version}" .
 
   patchVersion=$(docker run --rm --entrypoint=php larsnieuwenhuizen/php-fpm:${version} -v | pcregrep -o1 "${version}.([0-9]+)" | head -n 1)
   docker tag larsnieuwenhuizen/php-fpm:${version} larsnieuwenhuizen/php-fpm:${version}.${patchVersion}
